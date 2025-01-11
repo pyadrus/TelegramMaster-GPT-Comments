b4b68cde5d8a9aa259ae1de41d531a2c67fabaaa
@PyAdminRU <zh.vitaliy92@gmail.com>
2025-01-12 01:52:16 +0300

    Миграция с tkinter на Flet. Формирование главного меню программы.

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ from src.gui.app import action_1_with_log, action_2_with_log, action_3, action_4
 logger.add("user_data/log/log.log", rotation="1 MB", compression="zip")
 
 # Константы размеров окна
-WINDOW_WIDTH = 820
-WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 850 # ширина окна
+WINDOW_HEIGHT = 600 # высота окна
+
 
 class MainMenu:
     """Класс для отображения главного меню."""
 
-    def __init__(self, page: ft.Page, info_field: ft.TextField):
-        self.page = page
-        self.info_field = info_field
-        self.button_width = 700
-        self.button_height = 50
+    def __init__(self, page: ft.Page, info_list: ft.ListView):
+        self.page = page  # страница
+        self.info_list = info_list  # ListView для вывода текста
+        self.button_width = 700  # ширина кнопок
+        self.button_height = 60  # высота кнопок
 
     def create_title(self, text: str, font_size: int = 13) -> ft.Text:
         """Создает заголовок с градиентом."""
@@ -42,42 +43,57 @@ class MainMenu:
     def create_buttons(self) -> list:
         """Создает список кнопок меню."""
         return [
-            ft.ElevatedButton(
+            ft.OutlinedButton(
                 text="Получение списка каналов",
-                on_click=lambda _: action_1_with_log(self.page, self.info_field),
+                on_click=lambda _: action_1_with_log(self.page, self.info_list),
                 width=self.button_width,
                 height=self.button_height,
+                style=ft.ButtonStyle(
+                    shape=ft.RoundedRectangleBorder(radius=10),
+                ),
             ),
-            ft.ElevatedButton(
+            ft.OutlinedButton(
                 text="Отправка комментариев",
-                on_click=lambda _: action_2_with_log(self.page, self.info_field),
+                on_click=lambda _: action_2_with_log(self.page, self.info_list),
                 width=self.button_width,
                 height=self.button_height,
+                style=ft.ButtonStyle(
+                    shape=ft.RoundedRectangleBorder(radius=10),
+                ),
             ),
-            ft.ElevatedButton(
+            ft.OutlinedButton(
                 text="Смена имени, описания, фото",
-                on_click=lambda _: action_3(self.page, self.info_field),
+                on_click=lambda _: action_3(self.page, self.info_list),
                 width=self.button_width,
                 height=self.button_height,
+                style=ft.ButtonStyle(
+                    shape=ft.RoundedRectangleBorder(radius=10),
+                ),
             ),
-            ft.ElevatedButton(
+            ft.OutlinedButton(
                 text="Подписка на каналы",
-                on_click=lambda _: action_4(self.page, self.info_field),
+                on_click=lambda _: action_4(self.page, self.info_list),
                 width=self.button_width,
                 height=self.button_height,
+                style=ft.ButtonStyle(
+                    shape=ft.RoundedRectangleBorder(radius=10),
+                ),
             ),
-            ft.ElevatedButton(
+            ft.OutlinedButton(
                 text="Формирование списка каналов",
-                on_click=lambda _: action_5(self.page, self.info_field),
+                on_click=lambda _: action_5(self.page, self.info_list),
                 width=self.button_width,
                 height=self.button_height,
+                style=ft.ButtonStyle(
+                    shape=ft.RoundedRectangleBorder(radius=10),
+                ),
             ),
         ]
 
     def build(self) -> ft.Column:
         """Создает колонку с заголовками и кнопками."""
-        title = self.create_title(program_name, font_size=15)
-        version = self.create_title(program_version)
+        title = self.create_title(program_name, font_size=13)
+        version = self.create_title(program_version, font_size=13)
         buttons = self.create_buttons()
         return ft.Column(
             [title, version, *buttons],
@@ -103,22 +119,21 @@ class Application:
         page.window.resizable = False
 
         # Поле для вывода информации
-        info_field = ft.TextField(
-            multiline=True,
-            expand=True,
-            width=300,
-            label="Информация",
-            value="Тут будет выводиться информация...",
-        )
+        info_list = ft.ListView(expand=True, spacing=10, padding=20, auto_scroll=True)
+
+        # Добавляем стартовое сообщение в ListView
+        info_list.controls.append(ft.Text("TelegramMaster Commentator 🚀\n\nTelegramMaster Commentator 🚀 - это программа для автоматической расставления комментариев в каналах Telegram, а также для работы с аккаунтами.💬\n\n"
+        "Проект доступен на GitHub: https://github.com/pyadrus/TelegramMaster_Commentator 📂\n\n"
+        "Контакт с разработчиком в Telegram: https://t.me/PyAdminRU 📲\n\n"
+        "Информация на канале: https://t.me/master_tg_d 📡",))
 
         # Создаем главное меню
-        menu = MainMenu(page, info_field).build()
+        menu = MainMenu(page, info_list).build()
 
-        # Создаем макет
         layout = ft.Row(
             [
-                ft.Container(menu, width=250, padding=20),
-                ft.Container(info_field, expand=True, padding=20),
+                ft.Container(menu, width=300, padding=20),
+                ft.Container(info_list, expand=True, padding=20),
             ],
             alignment=ft.MainAxisAlignment.START,
         )
